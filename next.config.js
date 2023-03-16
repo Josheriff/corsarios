ec7049aa6c3b37fdd436d0652f1d47487d8e44d7
Jose <jose.manuel@holafly.com>
2023-03-16 15:24:31 +0100

    change basepath

--- next.config.js
+++ next.config.js
@@ -1,11 +1,21 @@
 module.exports = {
     basePath: '/corsarios',
     swcMinify: true,
+    async redirects() {
+        return ([
+          {
+            source: '/',
+            destination: '/corsarios',
+            permanent: true,
+            basePath: false,
+          },
+        ]);
+      },
     webpack: (config) => {
         config.resolve.fallback = {
             fs: false,
             buffer: false,
         };
-        return config;
-    }    
+        return config;        
+    }        
 }
